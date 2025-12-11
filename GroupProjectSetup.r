8c5707eb8820b2bf1989dfa78968ae076c56649b
Hudson Double <hdouble@vassar.edu>
2025-12-11 13:25:01 -0500

    Update data analysis and visualization in R project
    Fixed source file name case in GroupProjectData.rmd, added exploratory analysis steps, color mapping for apps, and switched to interactive plot with ggplotly. Moved comment in GroupProjectSetup.r for clarity.

--- GroupProjectSetup.r
+++ GroupProjectSetup.r
@@ -1,4 +1,5 @@
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, ggplot2, bslib, ggpubr, dplyr) # Pacman functions explained: https://www.epirhandbook.com/en/new_pages/basics.html
+pacman::p_load(tidyverse, ggplot2, bslib, ggpubr, dplyr) 
+# Pacman functions explained: https://www.epirhandbook.com/en/new_pages/basics.html
 
 Social_Media_Data <- read.csv("Students Social Media Addiction.csv") # Reads CSV Data
